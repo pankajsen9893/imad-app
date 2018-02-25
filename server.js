e21d7ffd2d8943fe7fe0446da93a5f071023a2c5
pankajsen9893 <34803475+pankajsen9893@users.noreply.github.com>
2018-02-25 18:23:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,20 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+//from here new web pages are added
+
+//first web page
+app.get('/firstWebPage', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'firstwebPage.html'));
+});
+//second web page
+app.get('/secondWebPage', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'secondWebPage.html'));
+});
+//third webpage
+app.get('/thirdWebPage', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'thirdWebPage.html'));
+});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
