7f2dfee436dcc1a64c616836c5476a96316ad19a
pankajsen9893 <34803475+pankajsen9893@users.noreply.github.com>
2018-02-27 12:42:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,6 +76,10 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 // using counter to set counter End Point
 var counter = 0;
 app.get('/counter', function(req, res){
